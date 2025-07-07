02c3f1b7b646897cb1005a3beb9f3903d5b10a04
MEL0RENZ <gomisha552@gmail.com>
2025-07-07 16:20:47 +0200

    Added my ava

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,7 +25,7 @@ function About({projects, certificates}) {
                     <div data-aos="fade-left">
                         <div  className='my_ava'>
                             <img
-                                // src="/images/myAva.png"
+                                src="/images/myAva.png"
                                 alt=""/>
                             <div className="pulse-ring"></div>
                         </div>
